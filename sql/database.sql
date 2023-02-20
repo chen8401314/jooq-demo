e49c2f772496e90f1bd3874749fc4612c23e8b26
chenxiang <328692993@qq.com>
2023-02-20 15:26:58 +0800

    [代码完善](master): springboot升级修改jooq代码生成方式

--- sql/database.sql
+++ sql/database.sql
@@ -4,14 +4,14 @@ CREATE TABLE pf_test_t
     create_time  DATETIME,
     age          INTEGER,
     birthday     DATE,
-    name         VARCHAR2(255),
+    name         VARCHAR2(255) COMMENT '名字',
     home_address VARCHAR2(255),
     event_status VARCHAR2(64),
     is_marry     BIT(1)
 );
 CREATE TABLE pf_user_t
 (
-    id        CHAR(32) NOT NULL PRIMARY KEY,
-    created_dt DATETIME,
-    name      VARCHAR2(255)
+    id         CHAR(32) NOT NULL PRIMARY KEY,
+    created_dt DATETIME COMMENT '创建时间',
+    name       VARCHAR2(255) COMMENT '名称'
 );
